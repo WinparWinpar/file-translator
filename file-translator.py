d1d50c1d95b5eb88a1f8284731a2a64a6880f99f
WinparWinpar <96503073+WinparWinpar@users.noreply.github.com>
2022-04-15 15:39:49 -0500

    Create file-translator.py

---
+++ file-translator.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+from deep_translator import GoogleTranslator
+import sys
+
+def touch(filename):
+    os.system(command=f'touch {filename}')
+
+class Translator:
+    def translate(source, lang, msg):
+        return GoogleTranslator(source=source, target=lang).translate(msg)
+
+def main(args: String[]):
+    try:
+        touch(args[2])
+    except:
+        try:
+            with open(args[0], 'r') as f:
+                file = f.readlines()
+                f.close()
+        except:
+            print(f'FileError: Cannot open file {args[0]}')
+            sys.exit(1)
+
+        try:
+            with open(args[2], 'w') as f1:
+                for line in file:
+                    newline = Translator.translate('auto', args[1], line)
+                    f1.write(newline)
+                f1.close()
+        except:
+            print(f'FileError: Cannot open file {args[2]}')
+            sys.exit(1)
+
+    print('Translation Complete!')
+
+if __name__ == '__main__':
+    main(sys.argv)
